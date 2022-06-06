56c4ebc9505a17aeaa53bbee1a61c41d8c83146d
floumicaza <flo@platan.us>
2022-06-06 17:46:10 -0400

    working mail

--- app/mailers/example_mailer.rb
+++ app/mailers/example_mailer.rb
@@ -2,6 +2,6 @@ class ExampleMailer < ApplicationMailer
   DEFAULT_LIFESPAN_IN_HOURS = 24
   def example_mail
     @email = params[:email]
-    mail(from: 'admin@example.com', to: @email, subject: 'Welcome to Potassium')
+    mail(from: 'flo@platan.us', to: @email, subject: 'Welcome to Potassium')
   end
 end
